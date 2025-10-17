20df158420dab777cd93deb6f9aac41266b13220
ndiayekhardiata2024 <ndiayekhardiata2024@gmail.com>
2025-10-17 11:34:40 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/frontend/', // ✅ nécessaire pour Ingress
   server: {
     host: '0.0.0.0',
-    port: 3000, // correspond au port interne exposé dans Docker
+    port: 3000,
   }
 })
