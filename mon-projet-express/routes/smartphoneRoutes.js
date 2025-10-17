8fb95759946b9bfbd9fea731cf3842191b4fc27f
ndiayekhardiata2024 <ndiayekhardiata2024@gmail.com>
2025-10-17 09:44:21 +0000

    Update smartphoneRoutes.js

--- mon-projet-express/routes/smartphoneRoutes.js
+++ mon-projet-express/routes/smartphoneRoutes.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/smartphoneController');
-const checkDeleteCode = require('../middleware/checkCode');
 
-// ✅ Route principale pour le frontend
-router.get('/', controller.getAllSmartphones);  // => /api/products
+// Route racine pour frontend
+router.get('/', controller.getAllSmartphones);
 
-// Routes CRUD
-router.post('/smartphones', controller.addSmartphone);
+// Routes classiques
 router.get('/smartphones', controller.getAllSmartphones);
 router.get('/smartphones/:id', controller.getSmartphoneById);
+router.post('/smartphones', controller.addSmartphone);
 router.put('/smartphones/:id', controller.updateSmartphone);
 
 // Suppression avec vérification
+const checkDeleteCode = require('../middleware/checkCode');
 router.delete('/smartphones/:id', checkDeleteCode, controller.deleteSmartphone);
 
 module.exports = router;
