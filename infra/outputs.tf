625fa384565c38378faa9c366112e495e2e97054
ndiayekhardiata2024 <ndiayekhardiata2024@gmail.com>
2025-10-22 19:52:20 +0000

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,7 +1,9 @@
-output "ec2_instance_id" {
-  value = aws_instance.web.id
+output "ec2_public_ip" {
+  description = "Adresse IP publique de l’instance EC2"
+  value       = aws_instance.web.public_ip
 }
 
-output "dynamodb_table_name" {
-  value = aws_dynamodb_table.users.name
+output "rds_endpoint" {
+  description = "Endpoint de la base de données RDS"
+  value       = aws_db_instance.db.endpoint
 }
