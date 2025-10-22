680f007df3e9eb6f35393a2a05bdbb0e220279e6
ndiayekhardiata2024 <ndiayekhardiata2024@gmail.com>
2025-10-22 19:51:46 +0000

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,3 +1,40 @@
-variable "region" {}
-variable "ami_id" {}
-variable "instance_type" {}
+variable "region" {
+  description = "Région AWS"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "Plage d’adresses IP du VPC"
+  type        = string
+}
+
+variable "subnet_cidr" {
+  description = "Plage IP du subnet public"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "ID de l’AMI (Amazon Linux 2 ou Ubuntu)"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Type d’instance EC2"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Nom de la clé SSH (.pem) créée dans le sandbox"
+  type        = string
+}
+
+variable "db_user" {
+  description = "Nom d’utilisateur de la base de données"
+  type        = string
+}
+
+variable "db_pass" {
+  description = "Mot de passe de la base de données"
+  type        = string
+  sensitive   = true
+}
